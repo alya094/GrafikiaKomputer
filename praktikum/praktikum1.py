9a96698545a32cceeb6c234d02a7d6ee233e2896
alya094 <alyaef094@gmail.com>
2025-12-25 08:18:12 +0700

    Delete praktikum directory

--- praktikum/praktikum1.py
+++
@@ -1,4 +0,0 @@
-x = 50
-y = 100
-warna = "merah"
-print(f"koordinat titik ({x},{y}) dengan warna {warna}.")
--- praktikum/praktikum2.py
+++
@@ -1,5 +0,0 @@
-x = int(input("masukkan nilai x: "))
-y = int(input("masukkan nilai y: "))
-warna = input("masukkan warna titik:")
-
-print(f"Titik berada di ({x}), {y}) dan berwarna {warna}.")
